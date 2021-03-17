f4172b9133b93db0790194a61cc6e299614bf59a
Tom Black <tom.black06007@gmail.com>
2021-03-16 22:09:30 -0700

    Continued working on the CommandsController.cs file.

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -2,8 +2,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Commander.Controllers
 {
+    [Route("api/commands")]
+    [ApiController]
     public class CommandsController : ControllerBase
     {
-        
+
     }
 }
